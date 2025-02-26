e044ed8eaa159a9dc40b08dd252e6831d338fd26
DarekParodia <darekparodia@gmail.com>
2025-02-26 11:33:44 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Galeros
+
+Simple gallery where users can post their art for other to see.
---
+++ php/header.php
@@ -0,0 +1,15 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about.php">About</a></li>
+            <li><a href="">Github</a></li>
+        </ul>
+    </nav>
+    <nav>
+        <ul>
+            <li><a href="/login.php">Login</a></li>
+            <li><a href="/register.php">Register</a></li>
+        </ul>
+    </nav>
+</header>
